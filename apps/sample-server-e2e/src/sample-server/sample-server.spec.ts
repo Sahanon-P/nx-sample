c0052d7ec189839307e2680c1e9d25a606a3eeb2
Sahanon Phisetpakasit <pingotakulml@gmail.com>
2024-02-27 15:26:05 +0700

    add another test

--- apps/sample-server-e2e/src/sample-server/sample-server.spec.ts
+++ apps/sample-server-e2e/src/sample-server/sample-server.spec.ts
@@ -5,6 +5,14 @@ describe('GET /api', () => {
     const res = await axios.get(`/api`);
 
     expect(res.status).toBe(200);
+    expect(res.data).toEqual({ message: 'Hello API' });
+  });
+  it('should return another message', async () => {
+    const res = await axios.get(`/api/auth`);
+
+    expect(res.status).toBe(200);
+    expect(res.data).toEqual({ message: 'auth' });
+
     // expect(res.data).toEqual({ message: 'Hello API' });
   });
 });
