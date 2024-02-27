dd5e7bcc11e3825e7d109d0e248182537e7845d2
Sahanon Phisetpakasit <pingotakulml@gmail.com>
2024-02-27 14:56:22 +0700

    initial commit

---
+++ apps/sample-server-e2e/src/sample-server/sample-server.spec.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+describe('Start Server', () => {
+  beforeAll(async () => {
+    const 
+  })
+})
+describe('GET /api', () => {
+  it('should return a message', async () => {
+    const res = await axios.get(`/api`);
+
+    expect(res.status).toBe(200);
+    // expect(res.data).toEqual({ message: 'Hello API' });
+  });
+});
